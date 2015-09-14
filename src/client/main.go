00c38ba1ba841fda0abf10ca1a2a7517de8016b9
Slawosz Slawinski <slawosz@gmail.com>
2015-09-14 20:46:03 +0200

    first commit

---
+++ src/client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func main() {
+
+}
