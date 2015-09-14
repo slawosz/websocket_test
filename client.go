376c5585f13b52e5b54e29f69dbad30772299e24
Slawosz Slawinski <slawosz@gmail.com>
2015-09-14 21:40:27 +0200

    adjustments

--- client.go
+++ client.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-        _ "bufio"
-        "bytes"
-        "fmt"
-        "github.com/gorilla/websocket"
-        "io/ioutil"
-        "net"
-        "net/http"
-        _ "net/http/pprof"
-        "net/url"
-        "os"
-        "sync"
-        "time"
+	_ "bufio"
+	"bytes"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"net/url"
+	"os"
+	"sync"
+	"time"
 )
 
 var C = 1000
@@ -21,59 +21,62 @@ var C = 1000
 var m = &sync.Mutex{}
 
 func main() {
-        wg := &sync.WaitGroup{}
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+	wg := &sync.WaitGroup{}
 
-        for i := 0; i < C; i++ {
-                wg.Add(1)
-                go connect(i, wg)
-        }
-        wg.Wait()
+	for i := 0; i < C; i++ {
+		wg.Add(1)
+		go connect(i, wg)
+	}
+	wg.Wait()
 }
 
 func connect(cid int, wg *sync.WaitGroup) {
-        conn, err := net.Dial("tcp", "localhost:4000")
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-        u, err := url.Parse("http://localhost:4000/foobar")
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-        h := &http.Header{}
-        h.Add("Origin", "localhost:4000")
-        sc, _, err := websocket.NewClient(conn, u, *h, 0, 0)
-        defer sc.Close()
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-        var mid = 0
-        for {
-                time.Sleep(1 * time.Nanosecond)
-                if err = sc.WriteMessage(1, []byte("Hello from client")); err != nil {
-                        fmt.Println(err)
-                        return
-                }
-                _, p, err := sc.ReadMessage()
-                if err != nil {
-                        fmt.Println(err)
-                        return
-                }
-                //m.Lock()
-                sout := bytes.NewBuffer(make([]byte, 0))
-                fmt.Fprintf(sout, "[c-%v][m-%v] RCVD:", cid, mid)
-                fmt.Fprintf(sout, "%v\n", string(p))
-                //m.Unlock()
-                go ioutil.WriteFile(fmt.Sprintf("results/%v.%v", cid, mid), sout.Bytes(), os.ModePerm)
-                mid++
-                if mid == 100 {
-                        return
-                }
-        }
-        time.Sleep(5 * time.Second)
-        // sout.Flush()
-        time.Sleep(5 * time.Second)
-        wg.Done()
+	conn, err := net.Dial("tcp", "127.0.0.1:4000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	u, err := url.Parse("http://127.0.0.1:4000/foobar")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	h := &http.Header{}
+	h.Add("Origin", "localhost:4000")
+	sc, _, err := websocket.NewClient(conn, u, *h, 0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sc.Close()
+	var mid = 0
+	for {
+		time.Sleep(1 * time.Nanosecond)
+		if err = sc.WriteMessage(1, []byte("Hello from client")); err != nil {
+			fmt.Println(err)
+			return
+		}
+		_, p, err := sc.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//m.Lock()
+		sout := bytes.NewBuffer(make([]byte, 0))
+		fmt.Fprintf(sout, "[c-%v][m-%v] RCVD:", cid, mid)
+		fmt.Fprintf(sout, "%v\n", string(p))
+		//m.Unlock()
+		ioutil.WriteFile(fmt.Sprintf("results/%v.%v", cid, mid), sout.Bytes(), os.ModePerm)
+		mid++
+		if mid == 100 {
+			return
+		}
+	}
+	time.Sleep(5 * time.Second)
+	// sout.Flush()
+	time.Sleep(5 * time.Second)
+	wg.Done()
 }
--- server.go
+++ server.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-        "fmt"
-        "github.com/gorilla/websocket"
-        "net/http"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
 )
 
 var upgrader = websocket.Upgrader{
-        ReadBufferSize:  1024,
-        WriteBufferSize: 1024,
-        CheckOrigin:     func(r *http.Request) bool { return true },
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func main() {
-        http.HandleFunc("/foobar", func(w http.ResponseWriter, r *http.Request) {
-                conn, err := upgrader.Upgrade(w, r, nil)
-                if err != nil {
-                        fmt.Println(err)
-                        return
-                }
-                for {
-                        messageType, p, err := conn.ReadMessage()
-                        if err != nil {
-                                // fmt.Println(err)
-                                return
-                        }
-                        p = append([]byte("SERVER GOT: "), p...)
-                        if err = conn.WriteMessage(messageType, p); err != nil {
-                                fmt.Println(err)
-                                return
-                        }
-                }
-        })
+	http.HandleFunc("/foobar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf(".")
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for {
+			messageType, p, err := conn.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			p = append([]byte("SERVER GOT: "), p...)
+			if err = conn.WriteMessage(messageType, p); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+	})
 
-        fmt.Println("Starting server")
-        err := http.ListenAndServe("0.0.0.0:4000", nil)
-        if err != nil {
-                panic("ListenAndServe: " + err.Error())
-        }
+	fmt.Println("Starting server")
+	err := http.ListenAndServe("0.0.0.0:4000", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
